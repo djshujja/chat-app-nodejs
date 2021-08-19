6da58db3d99cffa1daea7f28ecff5f73ab7f786f
Shujja Ahmed <djshujja@pop-os.localdomain>
2021-08-19 18:17:28 +0500

    basic chat functionality

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const http = require('http');
 const app = express();
 var cors = require('cors')
 const server = http.createServer(app);
+
+
 const io = socketio(server,
 	{
 		cors: {
@@ -12,25 +14,31 @@ const io = socketio(server,
   	}
 	});
 const chatRoutes = require('./routes/chatRoutes.js')
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./routes/userRoutes.js')
+
 const port = process.env.PORT || 5000;
 app.use(cors())
 
 
 
 io.on('connection', socket => {
-
-	console.log("we have an we connection!!!")
-
 	socket.on('join', ({name, room}, cb) => {
-		console.log(name, room)
-		
-		const error = true;
-		if(error) cb({error: "error"})
-
+		const {user,error} = addUser({id: socket.id, name, room});
+		if(error) cb({error: "error"});
+		socket.emit('message', { user: 'admin', text: 'Welcome to chat-app!' });
+		socket.broadcast.to(user.room).emit('message', {user:'admin', text:`${user.name}, has joined!`});
+		socket.join(user.room);
+		cb();
 	})
 
+	socket.on('sendMessage', (msg, cb) => {
+		const user = getUser(socket.id);
+		io.to(user.room).emit('message', {user:user.name, text: msg});
+		cb();
+	});
 
-	socket.on('disconnect', () => {
+
+	socket.on('disconnectUser', () => {
 		console.log("disconnected!")
 	})
 
